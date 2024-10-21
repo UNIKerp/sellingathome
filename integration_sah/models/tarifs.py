7a64188988193e0e186126b3c0f4b3504bb421d8
UNIKerp <153507340+UNIKerp@users.noreply.github.com>
2024-10-21 16:17:25 +0000

    product_tmpl_id

--- integration_sah/models/tarifs.py
+++ integration_sah/models/tarifs.py
@@ -9,6 +9,31 @@ class Tarifs(models.Model):
 
     price_sah_id = fields.Char(string="ID Prix SAH")
 
+    @api.model
+    def create(self,vals):
+        res = super(Tarifs,self).create(vals)
+        headers = self.env['authentication.sah'].establish_connection()
+        url_add_price = 'https://demoapi.sellingathome.com/v1/Prices'
+        if res.product_tmpl_id:
+            values = {
+                "ProductId": res.product_tmpl_id.product_sah_id,
+                "BrandTaxRate": 2.1,
+                "BrandTaxName": "sample string 3",
+                "TwoLetterISOCode": "FR",
+                "PriceExclTax": 1.1,
+                "PriceInclTax": 1.1,
+                "ProductCost": 5.1,
+                "EcoTax": 6.1,
+                "IsDefault": true,
+            }
+            response = requests.get(url_add_price, json=values, headers=headers)
+            if response1.status_code == 200:
+                data = response.json()
+                res.price_sah_id =  str(data.get('Id'))
+        
+        return res
+
+
     def write(self,vals):
         product_id = self.id
         url_edit = f'https://demoapi.sellingathome.com/v1/Prices/{self.price_sah_id}'
@@ -25,5 +50,5 @@ class Tarifs(models.Model):
             # "RolePrices":,
         }
         requests.put(url_edit, headers=headers, json=values)
-        res = super(Tarifs,self).create(vals)
+        res = super(Tarifs,self).write(vals)
         return res
