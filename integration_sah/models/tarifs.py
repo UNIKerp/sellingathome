b5813c1de7ebfd8fdb3af828c68429e056748b46
UNIKerp <153507340+UNIKerp@users.noreply.github.com>
2024-10-22 10:25:44 +0000

    RolePrices

--- integration_sah/models/tarifs.py
+++ integration_sah/models/tarifs.py
@@ -19,45 +19,52 @@ class Tarifs(models.Model):
     price_sah_id = fields.Char(string="ID Prix SAH")
 
     @api.model_create_multi
-    def create(self,vals):
-        res = super(Tarifs,self).create(vals)
+    def create(self, vals):
+        res = super(Tarifs, self).create(vals)
         headers = self.env['authentication.sah'].establish_connection()
+
         if res.product_tmpl_id and res.pricelist_id:
             price_list_id = str(res.pricelist_id.price_list_sah_id)
             url = f'https://demoapi.sellingathome.com/v1/Prices/{price_list_id}'
-            product_id =  res.product_tmpl_id
-            _logger.info('============================= %s',res.date_start)
-          
-            start_date = res.date_start.isoformat(timespec='microseconds') + "+02:00"
-            end_date = res.date_end.isoformat(timespec='microseconds') + "+02:00"
+            product_id = res.product_tmpl_id
+            
+            # Log des informations pour déboguer
+            _logger.info('============================= ProductId: %s, Date Start: %s, Date End: %s', product_id.produit_sah_id, res.date_start, res.date_end)
+            
+            start_date = res.date_start.isoformat(timespec='microseconds') + "+02:00" if res.date_start else False
+            end_date = res.date_end.isoformat(timespec='microseconds') + "+02:00" if res.date_end else False
+
+            # Calcul du prix TTC en ajoutant la taxe
+            price_incl_tax = product_id.list_price * (1 + (product_id.taxes_id.amount / 100)) if product_id.taxes_id else product_id.list_price
 
             values = {
                 "ProductId": product_id.produit_sah_id,
                 "TwoLetterISOCode": "FR",
                 "PriceExclTax": product_id.list_price,
-                "PriceInclTax": product_id.list_price * (product_id.taxes_id.amount/100),
+                "PriceInclTax": price_incl_tax,
                 "ProductCost": product_id.standard_price,
                 "RolePrices": [
                     {
-                    "CustomerRoleId": 1,
-                    "Quantity": int(res.min_quantity),
-                    "NewPriceExclTax": res.fixed_price ,
-                    #"NewPriceInclTax": res.fixed_price * (product_id.taxes_id.amount/100),
-                    "StartDate": start_date or False,
-                    "EndDate": end_date or False,
-                    # "CombinationId": 1
+                        "CustomerRoleId": 1,
+                        "Quantity": int(res.min_quantity) if res.min_quantity else 1,
+                        "NewPriceExclTax": res.fixed_price if res.fixed_price else 0.0,
+                        "StartDate": start_date if start_date else False,
+                        "EndDate": end_date if end_date else False,
                     },
-                ]   
+                ]
             }
+
             response = requests.put(url, json=values, headers=headers)
+            
             if response.status_code == 200:
                 data = response.json()
-                res.price_sah_id =  data['RolePrices'][0]['Id']
-                _logger.info('=============================== %s ==== %s', res.price_sah_id,data )
+                res.price_sah_id = data['RolePrices'][0]['Id']
+                _logger.info('=============================== %s', data)
             else:
-                _logger.info('=============================== %s',response.text)
-        
+                _logger.info('=============================== Error Response: %s', response.text)
+
         return res
+
 
 
     def write(self,vals):
