4e601eef0fcbe950f13edb282a59a225aae79c4d
UNIKerp <153507340+UNIKerp@users.noreply.github.com>
2024-10-22 10:33:03 +0000

    RolePrices

--- integration_sah/models/tarifs.py
+++ integration_sah/models/tarifs.py
@@ -40,16 +40,16 @@ class Tarifs(models.Model):
             values = {
                 "ProductId": product_id.produit_sah_id,
                 "TwoLetterISOCode": "FR",
-                "PriceExclTax": product_id.list_price,
+                "PriceExclTax": 500.0,
                 "PriceInclTax": price_incl_tax,
                 "ProductCost": product_id.standard_price,
                 "RolePrices": [
                     {
                         "CustomerRoleId": 1,
-                        # "Quantity": int(res.min_quantity) if res.min_quantity else 1,
-                        # "NewPriceExclTax": res.fixed_price if res.fixed_price else 0.0,
-                        # "StartDate": start_date if start_date else False,
-                        # "EndDate": end_date if end_date else False,
+                        "Quantity": int(res.min_quantity) if res.min_quantity else 1,
+                        "NewPriceExclTax": res.fixed_price if res.fixed_price else 0.0,
+                        "StartDate": start_date if start_date else False,
+                        "EndDate": end_date if end_date else False,
                     },
                 ]
             }
