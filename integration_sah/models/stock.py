12e2ca44454b336b933f227e054aadf35d51b47d
UNIKerp <153507340+UNIKerp@users.noreply.github.com>
2024-10-22 13:26:55 +0000

    button_validate

--- integration_sah/models/stock.py
+++ integration_sah/models/stock.py
@@ -15,14 +15,12 @@ class StockPickingSAH(models.Model):
             for line in self.move_ids_without_package:
                 qty_available = line.product_id.product_tmpl_id.qty_available
                 qty_available = qty_available + line.quantity
-                _logger.info('=====================%s',qty_available)
                 url = 'https://demoapi.sellingathome.com/v1/Stocks'
                 headers = self.env['authentication.sah'].establish_connection()
                 values = {
                     "ProductId":   line.product_id.product_tmpl_id.produit_sah_id,
                     "ProductReference":  line.product_id.product_tmpl_id.default_code,
                     "StockQuantity": int(qty_available),
-                    #"StockQuantityComing":int(produit.virtual_available),
                 }
                 response = requests.put(url, headers=headers, json=values)
                 if response.status_code == 200:
