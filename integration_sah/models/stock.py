64aeaaab54c299b96c55f2c78d08c53e106c03be
UNIKerp <153507340+UNIKerp@users.noreply.github.com>
2024-10-22 13:17:55 +0000

    button_validate

--- integration_sah/models/stock.py
+++ integration_sah/models/stock.py
@@ -6,6 +6,29 @@ from datetime import datetime
 
 _logger = logging.getLogger(__name__)
 
+class StockPickingSAH(models.Model):
+    _inherit = "stock.picking"
+
+    def button_validate(self):
+        res = super(StockPickingSAH,self).button_validate()
+        if self.move_ids_without_package:
+            for line self.move_ids_without_package:
+                qty_available = line.product_id.product_tmpl_id.qty_available
+                qty_available = qty_available+product_uom_qty
+                url = 'https://demoapi.sellingathome.com/v1/Stocks'
+                headers = self.env['authentication.sah'].establish_connection()
+                values = {
+                    "ProductId":   line.product_id.product_tmpl_id.produit_sah_id,
+                    "ProductReference":  line.product_id.product_tmpl_id.default_code,
+                    "StockQuantity": int(qty_available),
+                    #"StockQuantityComing":int(produit.virtual_available),
+                    }
+                    response = requests.put(url, headers=headers, json=values)
+                    if response.status_code == 200:
+                        print(response.json())  
+                    else:
+                        print(f"Erreur {response.status_code}: {response.text}")
+        return res
 
 class StockSAH(models.TransientModel):
     _inherit = "stock.change.product.qty"
