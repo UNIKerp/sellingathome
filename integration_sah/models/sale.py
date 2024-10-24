cca0fee17fa6f66a93ff7c5cb057fc7f74240fb8
sbadiane <sbadiane@unikerp.com>
2024-10-24 15:51:16 +0000

    test

--- integration_sah/models/sale.py
+++ integration_sah/models/sale.py
@@ -21,30 +21,11 @@ class SaleSAH(models.Model):
             _logger.info("ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc")
             _logger.info(commandes_sah)
             for commande in commandes_sah:
-                print('ppppppppppppppppppppppppppppppppppp',commande)
                 id_order = commande['Id']
                 commandes_odoo = self.env['sale.order'].search([('id_order_sh','=',id_order)])
                 client_id = self.env['res.partner'].search([('id_client_sah','=',commande['Customer']['Id'])])
                 # vendeur_id = self.env['res.users'].search([('id_vendeur_sah','=',commande['Seller']['Id'])])
-                _logger.info("maaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-                _logger.info(commandes_odoo)
-                _logger.info("nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn")
-                _logger.info(client_id)
-                _logger.info("partner_idpartner_idpartner_idpartner_idpartner_idpartner_id")
                 if not commandes_odoo and client_id:
-                    print("tttttttttttttttttttttttttttttttttttttttt")
-                    # delivery_address = self.env['res.partner'].create({
-                    #     'type':"delivery",
-                    #     'name': commande['FirstName']+'  '+commande['LastName'], 
-                    #     'phone': commande['phone'],
-                    #     'mobile': commande['MobilePhone'], 
-                    #     'street':commande['StreetAddress'],
-                    #     'street2':commande['StreetAddress2'],
-                    #     'city':commande['City'],
-
-                    # })
-                    # print("delivery_addressdelivery_addressdelivery_address",delivery_address)
-                    tax_model = self.env['account.tax']
                     commandes_odoo.create({
                         "id_order_sh":commande['Id'],
                         "name":commande['OrderRefCode'],
@@ -59,15 +40,23 @@ class SaleSAH(models.Model):
                         # "partner_shipping_id":delivery_address.id
                       
                     })
-                    _logger.info('mppppppppppppppppppppppppppppppppppppppppppppppppppp')
-                    _logger.info(commandes_odoo)
-                 
+                elif commandes_odoo:
+                    _logger.info("poooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo")
+                    commandes_odoo.write({
+                        "name":commande['OrderRefCode'],
+                        "partner_id":client_id.id,
+                        "user_id":client_id.user_id,
+                        'order_line': [(0, 0, {
+                            'product_id': self.env['product.template'].search([('produit_sah_id','=',elt['ProductId'])]).id or 1, 
+                            'product_uom_qty': elt['Quantity'],
+                            'price_unit': elt['UnitPrice'], 
+                            'tax_id': [(6, 0, [self._get_or_create_tax(elt['TaxRate'])])],
+                        }) for elt in commande['Products']] ,
+                    })
+
         else:
             print(f"Erreur {response.status_code}: {response.text}")
        
-
-
-
             
     def _get_or_create_tax(self, tax_rate):
         # Recherche la taxe par son montant
