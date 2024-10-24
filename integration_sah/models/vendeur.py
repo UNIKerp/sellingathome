274b036a36f0f7ef51a3859f41cc85660b017574
MorTallaFall <mtfall@unikerp.com>
2024-10-24 12:44:13 +0000

    resuser

--- integration_sah/models/vendeur.py
+++ integration_sah/models/vendeur.py
@@ -6,12 +6,13 @@ _logger = logging.getLogger(__name__)
 from datetime import datetime
 
 class vendeur(models.Model):
-    _inherit = "res.partner"
+    _inherit = "res.users"
 
     id_vendeur_sah = fields.Char(string='Id vendeur SAH')
-    is_seller = fields.Boolean(string='Est vendeur')
-
-
+    _sql_constraints = [
+        ('id_vendeur_sah_unique', 'unique(id_vendeur_sah)', 'Le champ "Id vendeur SAH" doit être unique.'),
+    ]
+    
     def recuperation_vendeurs_sah_vers_odoo(self):
         headers = self.env['authentication.sah'].establish_connection()
         url = 'https://demoapi.sellingathome.com/v1/Sellers'
@@ -19,11 +20,10 @@ class vendeur(models.Model):
         if response.status_code == 200:
             datas = response.json()
             for data in datas:
-                contact = self.env['res.partner'].search([('id_vendeur_sah','=',data['Id'])])
+                contact = self.env['res.users'].search([('login','=',data['Email'])])
                 if contact:
                     contact.write({
-                        'is_seller':True,
-                        'email':data['Email'],
+                        'id_vendeur_sah':data['Id'],
                         'phone':data['Phone'],
                         'mobile':data['MobilePhone'],
                         'street':data['StreetAddress'],
@@ -35,6 +35,67 @@ class vendeur(models.Model):
                         'company_name':data['CompanyName'],
                         'lang':data['Language']['ISOValue'],
                         'vat':data['CompanyVAT'],
+                        # 'ImageUrl':data[''],
+                        # 'Status':data[''],
+                        # 'StatusForever':data[''],
+                        # 'CandidacyId':data[''],
+                        # 'EmailIsFlagged':data[''],
+                        # 'Birthdate':data[''],
+                        # 'BirthPlace':data[''],
+                        # 'ParentSeller':data[''],
+                        # 'AnimatorSeller':data[''],
+                        # 'CustomerAccount':data[''],
+                        # 'NationalIdentificationNumber':data[''],
+                        # 'IdentityCardNumber':data[''],
+                        # 'Nationality':data[''],
+                        # 'CompanyStatus':data[''],
+                        # 'CompanyIdentificationNumber':data[''],
+                        # 'SocialContributionsType':data[''],
+                        # 'StartContractDate':data[''],
+                        # 'EndContractDate':data[''],
+                        # 'StartActivityDate':data[''],
+                        # 'RemoteStatus':data[''],
+                        # 'CreateCustomerAccount':data[''],
+                        # 'Brand':data[''],
+                        # 'Roles':data[''],
+                        # 'AdditionalInformations':data[''],
+                        # 'AccountBankCode':data[''],
+                        # 'AccountWicketCode':data[''],
+                        # 'AccountNumber':data[''],
+                        # 'AccountKey':data[''],
+                        # 'AccountIban'('id_vendeur_sah','=',data['Id']),:data[''],
+                        # 'AccountSwiftCode':data[''],
+                        # 'BankName':data[''],
+                        # 'BankingDomiciliation':data[''],
+                        # 'BankAccountOwner':data[''],
+                        # 'GdprAccepted':data[''],
+                        # 'AuthorizeSellerDeliveryModeOnEcommerce':data[''],
+                        # 'GdprLastAcceptedDate':data[''],
+                        # 'Photo':data[''],
+                        # 'TimeZone':data[''],
+                        # 'Signature':data[''],
+                        # 'MiniSiteUrl':data[''],
+                        # 'MiniSiteUsername':data[''],
+                        # 'MiniSiteIsActive':data[''],
+                    })
+                    print('RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR')
+                else:
+                    kk=contact.create({
+                        'id_vendeur_sah':data['Id'],
+                        'name':data['FirstName']+''+data['LastName'],
+                        'login':data['Email'],
+                        
+                        # 'phone':data['Phone'],
+                        # 'mobile':data['MobilePhone'],
+                        # 'street':data['StreetAddress'],
+                        # 'street2':data['StreetAddress2'],
+                        # 'zip':data['Postcode'],
+                        # 'city':data['City'],
+                        # 'partner_latitude':data['Latitude'],
+                        # 'partner_longitude':data['Longitude'],
+                        # 'company_name':data['CompanyName'],
+                        # 'lang':data['Language']['ISOValue'],
+                        # 'vat':data['CompanyVAT'],
                         # 'ImageUrl':data[''],
                         # 'Status':data[''],
                         # 'StatusForever':data[''],
@@ -78,5 +139,8 @@ class vendeur(models.Model):
                         # 'MiniSiteUsername':data[''],
                         # 'MiniSiteIsActive':data[''],
                     })
+                    if kk:
+                        print("YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY")
+
         else:
             _logger.info("==================================Erreur: %s ==========================",  response2.text)
