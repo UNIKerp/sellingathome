bdd29ff58076fa495be50468500389762d609b7e
UNIKerp <153507340+UNIKerp@users.noreply.github.com>
2024-10-28 13:36:32 +0000

    pricelist_id

--- integration_sah/models/tarifs.py
+++ integration_sah/models/tarifs.py
@@ -20,10 +20,8 @@ class Tarifs(models.Model):
 
     # redéfintion de la fonction _default_pricelist_id
     def _default_pricelist_id(self):
-        _logger.info("============================")
         active_id = self.env.context.get('active_id')
         product_id = self.env['product.template'].search([('id','=',active_id)])
-        _logger.info("===========================%s===%s",active_id,product_id)
         if product_id and product_id.default_list_price:
             return product_id.default_list_price
         else:
@@ -31,12 +29,12 @@ class Tarifs(models.Model):
                 '|', ('company_id', '=', False),
                 ('company_id', '=', self.env.company.id)], limit=1)
 
-    # pricelist_id = fields.Many2one(
-    #     comodel_name='product.pricelist',
-    #     string="Pricelist",
-    #     index=True, ondelete='cascade',
-    #     required=True,
-    #     default=_default_pricelist_id)
+    pricelist_id = fields.Many2one(
+        comodel_name='product.pricelist',
+        string="Pricelist",
+        index=True, ondelete='cascade',
+        required=True,
+        default=_default_pricelist_id)
 
     @api.model_create_multi
     def create(self, vals):
