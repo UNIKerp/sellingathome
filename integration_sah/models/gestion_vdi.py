1573a61a0bf1663c5f3205c4b7db9d8e4d190661
aliadilanielfayaz <aliadilanielfayaz@yahoo.fr>
2024-12-04 10:53:20 +0000

    maj de la table gestion_vdi

--- integration_sah/models/gestion_vdi.py
+++ integration_sah/models/gestion_vdi.py
@@ -11,10 +11,12 @@ _logger = logging.getLogger(__name__)
 
 class GestionVdi(models.Model):
     _name = "gestion.vdi"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "gestion vdi"
 
 
-    contact_vdi_ids = fields.Many2many('res.partner', string='Contacts vdi',required=True, help="Contacts vdi")
-    contact_vdi_master = fields.Many2one('res.partner', string='Contact vdi master',required=True, help="Contacts vdi master")
+    name = fields.Char(string='Nom du groupe',help="Contacts vdi")
+    contact_vdi_ids = fields.Many2many('res.partner', string='Contacts vdi',required=True, help="Contacts vdi", domain="[('vdi_id', '!=', False)]")
+    contact_vdi_master = fields.Many2one('res.partner', string='Contact vdi master',required=True, help="Contacts vdi master", domain="[('id', 'in', contact_vdi_ids)]")
     adresse_livraison = fields.Many2one('res.partner', string='Adresse de livraison',required=True,help="Adresse de livraison")
     
