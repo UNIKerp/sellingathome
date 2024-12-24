79ff461b753e6212dcf02c5787e5205c73e7040c
Hafizoune <mamadouhafizka@gmail.com>
2024-12-24 10:47:44 +0000

    product_image_url

--- integration_sah/models/produit.py
+++ integration_sah/models/produit.py
@@ -434,7 +434,7 @@ class ProduitSelligHome(models.Model):
             self.with_delay(**job_kwargs).creation_produit_odoo_sah(res,res.is_published,res.type,res.allow_out_of_stock_order,res.sale_ok,res.is_storable,res.categ_id,
                                     res.discountStartDate,res.discountEndDate,res.default_code,res.id,res.name,res.list_price,res.taxes_id,
                                     res.standard_price,res.barcode,res.weight,res.long_sah,res.haut_sah,res.availableOnHostMinisites,
-                                    res.description,res.accessory_product_ids,res.attribute_line_ids,product_image_url)
+                                    res.description,res.accessory_product_ids,res.attribute_line_ids,product_photos)
         return res
 
 
