7b3ec6894df5c0505c7888f8ef8ac00de19ea70c
UNIKerp <153507340+UNIKerp@users.noreply.github.com>
2024-12-26 17:59:42 +0000

    product_id.produit_sah_id

--- integration_sah/models/produit.py
+++ integration_sah/models/produit.py
@@ -523,7 +523,7 @@ class ProduitSelligHome(models.Model):
             for photo in values['ProductPhotos']:
                 photo["Link"] = "https://demoseller.sellingathome.com/api/s3/sah-files/products/120781/87-26-2f-33-ed-3a-21-f5-62-12-4a-20-34-7f-27-1e-98-76-e9-76.png"
             _logger.info('================================= aprss%s',values)
-            url_put = f"https://demoapi.sellingathome.com/v1/Products/120781"
+            url_put = f"https://demoapi.sellingathome.com/v1/Products/{product_id.produit_sah_id}"
             _logger.info('result======================================%s',url_put)
             result = requests.put(url_put,json=values, headers=headers)
             _logger.info('================================= result%s',result)
