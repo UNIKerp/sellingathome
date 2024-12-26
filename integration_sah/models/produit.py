bf003eab989aaa7e3f805d8d1c2d2bb3038b5b4f
UNIKerp <153507340+UNIKerp@users.noreply.github.com>
2024-12-26 14:35:10 +0000

    product_photos

--- integration_sah/models/produit.py
+++ integration_sah/models/produit.py
@@ -9,10 +9,10 @@ from odoo.tools import config
 import pytz
 import logging
 _logger = logging.getLogger(__name__)
-import re
+
 from PIL import Image
 from io import BytesIO
-is_product_sah = 0
+
 class ProduitSelligHome(models.Model):
     _inherit = "product.template"
 
@@ -278,10 +278,7 @@ class ProduitSelligHome(models.Model):
         id_categ = ''
         categ_parent =''
         suivi_stock = 1 if is_storable == True else 0
-        global is_product_sah
-        is_product_sah = is_product_sah+1
-        if categ_id and not objet.produit_sah_id and is_product_sah == 0:
-            _logger.info("############################################### DEBUT CREATION %s #################################")
+        if categ_id and not objet.produit_sah_id:
             url_categ = "https://demoapi.sellingathome.com/v1/Categories"
             post_response_categ = requests.get(url_categ, headers=headers)
             
@@ -414,12 +411,7 @@ class ProduitSelligHome(models.Model):
             if post_response.status_code == 200:
                 response_data = post_response.json()
                 product_id = response_data.get('Id')
-                _logger.info('=======================================%s',product_id)
                 objet.produit_sah_id = int(product_id)
-                
-                _logger.info('======================================= SHA%s',   objet.produit_sah_id)
-        if is_product_sah >= 1:
-            is_product_sah = 0
 
     @api.model
     def create(self, vals):
@@ -451,16 +443,10 @@ class ProduitSelligHome(models.Model):
             job_kwargs = {
                 'description': 'Création produit Odoo vers SAH',
             }
-            reponse = res.with_delay(**job_kwargs).creation_produit_odoo_sah(res,res.is_published,res.type,res.allow_out_of_stock_order,res.sale_ok,res.is_storable,res.categ_id,
+            res.with_delay(**job_kwargs).creation_produit_odoo_sah(res,res.is_published,res.type,res.allow_out_of_stock_order,res.sale_ok,res.is_storable,res.categ_id,
                                     res.discountStartDate,res.discountEndDate,res.default_code,res.id,res.name,res.list_price,res.taxes_id,
                                     res.standard_price,res.barcode,res.weight,res.long_sah,res.haut_sah,res.availableOnHostMinisites,
                                     res.description,res.accessory_product_ids,res.attribute_line_ids,product_photos)
-            _logger.info('================================ reponse %s',reponse)
-            # pattern = r'\b([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})\b'
-            # match = re.search(pattern, texte)
-            # if match:
-            #     uuid = match.group(1)
-
         return res
 
     def write(self, vals):
