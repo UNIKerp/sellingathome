dcedb33bafbc023f394b5045b8b1ebd6646c7fad
UNIKerp <153507340+UNIKerp@users.noreply.github.com>
2024-12-26 14:13:40 +0000

    headers

--- integration_sah/models/produit.py
+++ integration_sah/models/produit.py
@@ -460,20 +460,22 @@ class ProduitSelligHome(models.Model):
         return res
 
     def write(self, vals):
-        headers = self.env['authentication.sah'].establish_connection()
-        rec = super(ProduitSelligHome, self).write(vals)
-        if vals and self.produit_sah_id:
-            job_kwargs = {
-                'description': 'Mise à jour du produit dans SAH',
-            }
-            self.with_delay(**job_kwargs).update_produit_dans_sah(self, headers)
+        if self.id:
+            _logger.info('======================================',self.id)
+            headers = self.env['authentication.sah'].establish_connection()
+            rec = super(ProduitSelligHome, self).write(vals)
+            if vals and self.produit_sah_id:
+                job_kwargs = {
+                    'description': 'Mise à jour du produit dans SAH',
+                }
+                self.with_delay(**job_kwargs).update_produit_dans_sah(self, headers)
 
-            ### Modification stock
-            job_kwargs2 = {
-                'description': 'Mise à jour du stock produit',
-            }
-            self.with_delay(**job_kwargs2).maj_des_stocks(self.is_storable,self.produit_sah_id,self.default_code,self.qty_available,self.virtual_available)
-        return rec
+                ### Modification stock
+                job_kwargs2 = {
+                    'description': 'Mise à jour du stock produit',
+                }
+                self.with_delay(**job_kwargs2).maj_des_stocks(self.is_storable,self.produit_sah_id,self.default_code,self.qty_available,self.virtual_available)
+            return rec
 
     def maj_des_stocks(self,is_storable,produit_sah_id,default_code,qty_available,virtual_available):
         headers = self.env['authentication.sah'].establish_connection()
