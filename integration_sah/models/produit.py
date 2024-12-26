a370875159bd04e5b2818b2df704283ea8aa0f02
UNIKerp <153507340+UNIKerp@users.noreply.github.com>
2024-12-26 17:35:23 +0000

    values['ProductPhotos']

--- integration_sah/models/produit.py
+++ integration_sah/models/produit.py
@@ -520,7 +520,8 @@ class ProduitSelligHome(models.Model):
                     values = elt
                     break
             _logger.info('=================================%s',values)
-            values['ProductPhotos']['Link'] = "https://unikerp-sellingathome-staging-17258348.dev.odoo.com/web/content/2527/product_image_317.png"
+            for photo in values['ProductPhotos']:
+                photo["Link"] = "https://unikerp-sellingathome-staging-17258348.dev.odoo.com/web/content/2527/product_image_317.png"
             _logger.info('================================= aprss%s',values)
             url_put = f"https://demoapi.sellingathome.com/v1/Products{product_id.produit_sah_id}"
             result = requests.put(url_produit,json=values, headers=headers)
