041e55241931176aedd2f60beddc81574cbf3bd9
UNIKerp <153507340+UNIKerp@users.noreply.github.com>
2024-12-26 12:37:27 +0000

    cc

--- integration_sah/__manifest__.py
+++ integration_sah/__manifest__.py
@@ -18,7 +18,7 @@
         'views/gestion_vdi.xml',
         'views/methode_paiement.xml',
         'views/model_paiement.xml',
-        # 'data/methode_paiement_sah.xml',
+        'data/methode_paiement_sah.xml',
     ],
     'images':['static/description/icon.png',],
 }
