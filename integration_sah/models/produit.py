448cf63c53b7199aea2c50f366c658ef75abb6e9
UNIKerp <153507340+UNIKerp@users.noreply.github.com>
2024-12-27 18:02:25 +0000

    ggghh

--- integration_sah/models/produit.py
+++ integration_sah/models/produit.py
@@ -239,7 +239,7 @@ class ProduitSelligHome(models.Model):
         else:
             discount_end_date = None
        
-        lisp ={'Link':'https://demoseller.sellingathome.com/api/s3/sah-files/products/120834/9b-9c-4b-8d-61-f7-10-cd-d0-34-28-f4-23-35-69-ce-76-d7-13-de.jpg'}
+        lisp ={'Link':'https://unikerp-sellingathome-staging-17258348.dev.odoo.com/web/content/3143/product_image_507.jpg'}
       
         product_data = {
             "ProductType": 5,
