908bf1ab6bbff98eaeb46ba4a7e4b2ebea1ff57b
MorTallaFall <159531994+MorTallaFall@users.noreply.github.com>
2024-12-27 13:24:58 +0000

    comm

--- integration_sah/models/nomenclature.py
+++ integration_sah/models/nomenclature.py
@@ -14,7 +14,8 @@ class NomenclatureSelligHome(models.Model):
     def create(self, vals):
         res = super(NomenclatureSelligHome, self).create(vals)
         if res:
-            self.creation_nomenclature_produits()
+            self.creation_nomenclature_produits(res)
+            print("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK")
         return res
     def write(self, vals):
         headers = self.env['authentication.sah'].establish_connection()
@@ -22,27 +23,29 @@ class NomenclatureSelligHome(models.Model):
         
         return rec
     
-    def creation_nomenclature_produits(self):
-        headers = self.env['authentication.sah'].establish_connection()
-        url_produit = "https://demoapi.sellingathome.com/v1/Products"
-        
-        post_response_produit = requests.get(url_produit, headers=headers, timeout=120)
-        
-        if post_response_produit.status_code == 200:
-            response_data_produit = post_response_produit.json()
-            print("YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY",response_data_produit)
-            data = {
-                "Name": "10% sur votre commande",
-                "ProductId": 119740,
-                "MaxQuantity": 1,
-                "Deleted": true,
-                "RemoteReference": "sample string 3",
-            }
-            response = requests.post(url_produit, json=data, headers=headers)
+    def creation_nomenclature_produits(self,res):
+        if res.product_tmpl_id.produit_sah_id:
+            headers = self.env['authentication.sah'].establish_connection()
+            url_produit = f"https://demoapi.sellingathome.com/v1/Products/{res.product_tmpl_id.produit_sah_id}"
+            post_response_produit = requests.get(url_produit, headers=headers, timeout=120)
+            _logger.info("IIIIIIIIIIjjjjjjjjjjjjjjjjjjjjjjjIIIIIIIIIIIIII%s",post_response_produit.status_code)
+            if post_response_produit.status_code == 200:
+                response_data_produit = post_response_produit.json()
+                _logger.info("IIIIIIIIIIjjjjjjjjjjjjjjjjjjjjjjjIIIIIIIIIIIIII%s",response_data_produit['ProductRelatedProducts'])
+                response_data_produit['AttachedProducts'] = [ {'GroupId': 119741, 'ProductId': 119708, 'ProductRemoteId': None, 'ProductCombinationId': 0, 'Quantity': 2, 'DisplayOrder': 1, 'Deleted': True } ]
+       
+                _logger.info('PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP%s',response_data_produit['ProductRelatedProducts'])
 
-            if response.status_code != 200:
-                raise Exception("Erreur lors de la création du produit sur SellingAtHome: %s" % response.text)
-
-            return product
+            
+                url_produit_modif = f"https://demoapi.sellingathome.com/v1/Products/{res.product_tmpl_id.produit_sah_id}"
+                try:
+                    response = requests.put(url_produit_modif, json= response_data_produit, headers=headers)
+                    _logger.info("Statut de la réponse : %s, Contenu : %s", response.status_code, response.text)
+                    if response.status_code != 200:
+                        raise Exception("Erreur lors de la modification du produit : %s" % response.text)
+                except requests.exceptions.RequestException as e:
+                    _logger.error("Erreur lors de la requête PUT : %s", str(e))
+                    raise
+                return product
 
 
