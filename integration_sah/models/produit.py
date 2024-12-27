2163e08d583c120163f375ef1e4e4d619ca57073
UNIKerp <153507340+UNIKerp@users.noreply.github.com>
2024-12-27 15:00:43 +0000

    headers

--- integration_sah/models/produit.py
+++ integration_sah/models/produit.py
@@ -407,7 +407,8 @@ class ProduitSelligHome(models.Model):
     def maj_images_du_produit(self,product_id):
         base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
         photos_produit = []
-        url = f"https://demoapi.sellingathome.com/v1/Products{product_id.produit_sah_id}" 
+        url = f"https://demoapi.sellingathome.com/v1/Products{product_id.produit_sah_id}"
+        headers = self.env['authentication.sah'].establish_connection()
         response =  requests.get(url, headers=headers)
         if response.status_code == 200:
             playload = response.json()
