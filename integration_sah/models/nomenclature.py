d2202770d3642bb0efd691e5f56442bb38a3df3d
MorTallaFall <159531994+MorTallaFall@users.noreply.github.com>
2024-12-30 17:21:53 +0000

    creation_nomenclature_produitsAttachedProducts

--- integration_sah/models/nomenclature.py
+++ integration_sah/models/nomenclature.py
@@ -10,20 +10,14 @@ _logger = logging.getLogger(__name__)
 class NomenclatureSelligHome(models.Model):
     _inherit = "mrp.bom"
 
-    @api.model
-    def create(self, vals):
-        res = super(NomenclatureSelligHome, self).create(vals)
-        if res:
-            self.creation_nomenclature_produits(res)
-            print("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK")
-        return res
+  
     def write(self, vals):
         headers = self.env['authentication.sah'].establish_connection()
         rec = super(NomenclatureSelligHome, self).write(vals)
-        
+        self.creation_nomenclature_produits(self,headers)
         return rec
     
-    def creation_nomenclature_produits(self,res):
+    def creation_nomenclature_produits(self,product_id,headers):
         if res.product_tmpl_id.produit_sah_id:
             headers = self.env['authentication.sah'].establish_connection()
             url_produit = f"https://demoapi.sellingathome.com/v1/Products/{res.product_tmpl_id.produit_sah_id}"
@@ -32,10 +26,25 @@ class NomenclatureSelligHome(models.Model):
             if post_response_produit.status_code == 200:
                 response_data_produit = post_response_produit.json()
                 _logger.info("IIIIIIIIIIjjjjjjjjjjjjjjjjjjjjjjjIIIIIIIIIIIIII%s",response_data_produit['AttachedProducts'])
-                response_data_produit['AttachedProducts'] = [ {'GroupId': 119741, 'ProductId': 119708, 'ProductRemoteId': None, 'ProductCombinationId': 0, 'Quantity': 2, 'DisplayOrder': 1, 'Deleted': True } ]
-                _logger.info('PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP%s',response_data_produit['AttachedProducts'])
-                url_produit_modif = f"https://demoapi.sellingathome.com/v1/Products/{res.product_tmpl_id.produit_sah_id}"
-                response = requests.put(url_produit_modif, json= response_data_produit, headers=headers)
+                datas = {
+                        {
+                        # "GroupId": 1,
+                        # "ProductId": 2,
+                        "Quantity": 2,
+                        "DisplayOrder": 1,
+                        "Deleted": True
+                        },
+                        {
+                        # "GroupId": 1,
+                        # "ProductId": 2,
+                        "Quantity": 4,
+                        "DisplayOrder": 1,
+                        "Deleted": True
+                        }
+                }
+                response_data_produit['AttachedProducts'] = datas
+               
+                response = requests.put(url_produit, json= response_data_produit, headers=headers)
                 _logger.info("Statut de la réponse : %s, Contenu : %s", response.status_code, response.text)
                 if response.status_code != 200:
                    _logger.info('Okkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk')
