a90a85e333c93a70eedc0fe29bbceb832574fa84
MorTallaFall <159531994+MorTallaFall@users.noreply.github.com>
2024-12-31 11:59:13 +0000

    rec

--- integration_sah/models/nomenclature.py
+++ integration_sah/models/nomenclature.py
@@ -53,7 +53,7 @@ class NomenclatureSelligHome(models.Model):
                 response = requests.put(url_produit, json= response_data_produit, headers=headers)
                 _logger.info("================================d Résultat final : %s",response)
     def creation_des_api_nsene(self,rec):
-        if res.product_tmpl_id.produit_sah_id:
+        if rec.product_tmpl_id.produit_sah_id:
             headers = self.env['authentication.sah'].establish_connection()
             url_produit = f"https://demoapi.sellingathome.com/v1/Products/{res.product_tmpl_id.produit_sah_id}"
             post_response_produit = requests.get(url_produit, headers=headers)
