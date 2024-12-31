857870e7e350f09e14031ea2ce5fbf107667c747
MorTallaFall <159531994+MorTallaFall@users.noreply.github.com>
2024-12-31 11:22:58 +0000

    test

--- integration_sah/models/nomenclature.py
+++ integration_sah/models/nomenclature.py
@@ -16,7 +16,7 @@ class NomenclatureSelligHome(models.Model):
         rec = super(NomenclatureSelligHome, self).create(vals)
         if rec:
             self.creation_nomenclature_produits(rec,headers)
-            self.test_modification_produits(rec,headers)
+            # self.test_modification_produits(rec,headers)
         return rec
     
     def write(self, vals):
