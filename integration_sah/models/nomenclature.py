d63f4d1f4787d276d01d9ce4494a75e6270ee33a
Hafizoune <mamadouhafizka@gmail.com>
2025-01-02 17:15:31 +0000

    liste_composant

--- integration_sah/models/nomenclature.py
+++ integration_sah/models/nomenclature.py
@@ -63,7 +63,7 @@ class NomenclatureSelligHome(models.Model):
                                  {
                                 # "GroupId": 1,
                                 "ProductId": line.product_id.produit_sah_id or 0,
-                                "Quantity": 9,
+                                "Quantity": int(line.product_qty),
                                 "DisplayOrder": 6,
                                 # "Deleted": True
                             } for line in res.bom_line_ids
