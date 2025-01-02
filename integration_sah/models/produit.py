900805380c7160745c0895b353c9dd94296fbe83
Hafizoune <mamadouhafizka@gmail.com>
2025-01-02 16:00:24 +0000

    type_produit_sah

--- integration_sah/models/produit.py
+++ integration_sah/models/produit.py
@@ -26,7 +26,7 @@ class ProduitSelligHome(models.Model):
     discountEndDate = fields.Datetime("Date Fin SAH", help="Date de fin dans SAH")
     discountStartDate = fields.Datetime("Date debut SAH", help="Date de début dans SAH")
     discountBadgeIsActive = fields.Boolean("BadgeEst Actif", help="Le badge de réduction est actif dans SAH")
-    type_produit_sah = fields.Selection[string="Type produit SAH",('5':'5'),('10':'10'),('20':'20'), default='5']
+    type_produit_sah = fields.Selection(selection=[('5':'5'),('10':'10'),('20':'20')], string="Type produit SAH", default='5')
 
     # url_image = fields.Char("URL image", help="Url de l'image")
 
