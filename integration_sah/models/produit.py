7728762645cb68f55f4001cb04d5b6e7da730d4a
Hafizoune <mamadouhafizka@gmail.com>
2025-01-02 15:43:33 +0000

    type groupé

--- integration_sah/models/produit.py
+++ integration_sah/models/produit.py
@@ -241,7 +241,7 @@ class ProduitSelligHome(models.Model):
             discount_end_date = None
       
         product_data = {
-            "ProductType": 5,
+            "ProductType": 10,
             "Reference": product_id.default_code,
             "Prices": [
                 {
