ada21ba9499abcfe9c73a1c012d56baeaa21494a
UNIKerp <153507340+UNIKerp@users.noreply.github.com>
2025-01-17 16:37:04 +0000

    AttachedProducts

--- integration_sah/models/produit.py
+++ integration_sah/models/produit.py
@@ -163,14 +163,14 @@ class ProduitSelligHome(models.Model):
                         'ISOValue': 'fr'
                     }
                 ],
-                "AttachedProducts": [
-                    {
-                    "ProductId": line.product_id.produit_sah_id or 0,
-                    "Quantity": int(line.product_qty),
-                    "DisplayOrder": 2,
-                    }
-                    for line in composants
-                ],
+                # "AttachedProducts": [
+                #     {
+                #     "ProductId": line.product_id.produit_sah_id or 0,
+                #     "Quantity": int(line.product_qty),
+                #     "DisplayOrder": 2,
+                #     }
+                #     for line in composants
+                # ],
                 "Categories": [
                     {
                         "Id": id_categ,
