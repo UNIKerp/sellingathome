42ecda559ddcd59dc09c47937aca88ade178a109
UNIKerp <153507340+UNIKerp@users.noreply.github.com>
2025-01-17 14:01:55 +0000

    update_produit_dans_sah

--- integration_sah/models/tarifs.py
+++ integration_sah/models/tarifs.py
@@ -57,6 +57,6 @@ class Tarifs(models.Model):
         job_kwargs = {
                 'description': 'Mise à jour liste de prix',
         }
-        self.env['product.template'].with_delay(**job_kwargs).update_produit_dans_sah(res.product_tmpl_id, headers)
+        self.env['product.template'].with_delay(**job_kwargs).update_produit_dans_sah(self.product_tmpl_id, headers)
         # self.with_delay(**job_kwargs).product_id.update_produit_dans_sah(self.product_tmpl_id, headers)
         return res
