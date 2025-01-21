470988022b1409e12b0ac65812fa640a9b5741ad
Hafizoune <mamadouhafizka@gmail.com>
2025-01-21 18:12:09 +0000

    type groupé

--- integration_sah/models/produit.py
+++ integration_sah/models/produit.py
@@ -144,7 +144,7 @@ class ProduitSelligHome(models.Model):
             _logger.info(f'============================={attached_products}=========================')
             url_produit = f"https://demoapi.sellingathome.com/v1/Products/{product.produit_sah_id}"
             update_data = {
-                "ProductType": 10,
+                "ProductType": product.type_produit_sah,
                 "Reference": product.default_code,
                 "Prices": [
                     {
