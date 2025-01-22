4b4a04f45d837cc03e0fbc58d0534b2e0f9f96a2
Hafizoune <mamadouhafizka@gmail.com>
2025-01-22 16:06:12 +0000

    creation_nomenclature_produits

--- integration_sah/models/nomenclature.py
+++ integration_sah/models/nomenclature.py
@@ -19,7 +19,7 @@ class NomenclatureSelligHome(models.Model):
             job_kwargs = {
             'description': 'Ajout des Nommenclatures du produit de Odoo vers SAH',
             }
-            self.env['product.template'].with_delay(**job_kwargs).update_produit_dans_sah(rec.product_tmpl_id, headers)
+            self.env['product.template'].with_delay(**job_kwargs).creation_nomenclature_produits(rec.product_tmpl_id, headers)
         return rec
     
     def write(self, vals):
