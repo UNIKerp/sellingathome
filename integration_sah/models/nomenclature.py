b772394afaf6a1e75b2c7c10dbb2a4c99ed7c3f5
Hafizoune <mamadouhafizka@gmail.com>
2025-01-23 16:29:45 +0000

    RolePrices

--- integration_sah/models/nomenclature.py
+++ integration_sah/models/nomenclature.py
@@ -97,10 +97,10 @@ class RolePricesSelligHome(models.Model):
         self.with_delay(**job_kwargs).creation_role_produits(self, headers)
         return rec
     
-    def creation_role_produits(self,res,headers):
-        if res.product_tmpl_id.produit_sah_id :
+    def creation_role_produits(self,rec,headers):
+        if rec.product_tmpl_id.produit_sah_id :
             headers = self.env['authentication.sah'].establish_connection()
-            url_produit = f"https://demoapi.sellingathome.com/v1/Products/{res.product_tmpl_id.produit_sah_id}"
+            url_produit = f"https://demoapi.sellingathome.com/v1/Products/{rec.product_tmpl_id.produit_sah_id}"
             post_response_produit = requests.get(url_produit, headers=headers)
             if post_response_produit.status_code == 200:
                 response_data_produit = post_response_produit.json()
