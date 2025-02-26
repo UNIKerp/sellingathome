36ff130855c04e7e45b7585075bfa4846d8ef6d1
UNIKerp <153507340+UNIKerp@users.noreply.github.com>
2025-02-26 16:15:56 +0000

    readonly="1" force_save="1"

--- integration_sah/models/methode_paiement.py
+++ integration_sah/models/methode_paiement.py
@@ -7,16 +7,27 @@ _logger = logging.getLogger(__name__)
 
 class MethodePaiement(models.Model):
     _name = "methode.paiement.sah"
-    _inherit = ['mail.thread', 'mail.activity.mixin']
-    _description = "gestion des methodes de paiement"
+    _description = "gestion des methodes de paiement sah"
 
     name = fields.Char(string='Nom',required=True, copy=False)
     code = fields.Char(string='Code',required=True, copy=False)
     value = fields.Integer( copy=False)
-    is_confirme = fields.Boolean(string='Confirmer Commande',help="Le paiement est confirmé")
+    is_confirme = fields.Boolean(string='Confirmer Commande',help="Si les commandes rattachées sont automatiquement confirmées.")
     
     _sql_constraints = [
         ('code_uniq', 'unique (code)', "Ce code exists deja!")]
+
+
+class ModeLivraison(models.Model):
+    _name = "mode.livraison.sah"
+    _description = "gestion des Modes de livraison SAH"
+
+    name = fields.Char(string='Nom',required=True, copy=False)
+    desciption = fields.Char(string='Description')
+    value = fields.Integer( string='Valeur' , copy=False , required=True)
+    
+    _sql_constraints = [
+        ('value_uniq', 'unique (value)', "Cette Valeur exists deja!")]
  
 
      
--- integration_sah/models/sale.py
+++ integration_sah/models/sale.py
@@ -22,6 +22,7 @@ class SaleSAH(models.Model):
     vdi = fields.Many2one('res.partner',string="Ambassadeur",help='le reveudeur vdi dans SAH')
     methode_paiement_id = fields.Many2one('methode.paiement.sah',string="Moyen de paiement",help='la Methode de paiement')
     paiement_ids = fields.One2many('paiement.sah','order_id',string="Paiement SAH",help='le paiement dans SAH')
+    mode_livraison_sah_id = fields.Many2one('mode.livraison.sah',string="Mode de transport")
     _sql_constraints = [
         ('id_order_sh_uniq', 'unique (id_order_sh)', "ID commande SAH exists deja!"), ]
 
