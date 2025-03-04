45c99003b6d9adaa266cd6643c055a29086aaa78
UNIKerp <153507340+UNIKerp@users.noreply.github.com>
2025-03-04 15:27:13 +0000

    order_map

--- integration_sah/models/sale_sah_mapping.py
+++ integration_sah/models/sale_sah_mapping.py
@@ -49,7 +49,7 @@ class MappingSAHOdoo(models.Model):
                             job_kwargs_commandes = {
                                 "description": "Création d'une nouvelle commande de SAH vers Odoo",
                             }
-                            self.with_delay(**job_kwargs_commandes).get_commande(order_map)                
+                            order_map.with_delay(**job_kwargs_commandes).get_commande(commande)                
     
     def get_commande(self,commande):
         if commande :  
