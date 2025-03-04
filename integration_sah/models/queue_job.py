434aa430c7ac03175af17c64da4c86b179464850
UNIKerp <153507340+UNIKerp@users.noreply.github.com>
2025-03-04 15:36:14 +0000

    commande_sah_id

--- integration_sah/models/queue_job.py
+++ integration_sah/models/queue_job.py
@@ -18,9 +18,13 @@ class QueueJob(models.Model):
 
     def _set_integration(self):
         for rec in self:
+            _logger.info("11111111111111111")
             odoo_model = rec.records[:1]
+            _logger.info("222222222 %s",odoo_model)
             value = False
             if odoo_model and hasattr(odoo_model, '_get_integration_id_for_job'):
+
+                _logger.info("33333333333")
                 value = odoo_model.exists()._get_integration_id_for_job()
 
             rec.commande_sah_id = value
@@ -28,5 +32,9 @@ class QueueJob(models.Model):
     @api.model_create_multi
     def create(self, vals_list):
         records = super(QueueJob, self).create(vals_list)
+
+        _logger.info("444444444")
         records._set_integration()
+
+        _logger.info("555555555")
         return records
