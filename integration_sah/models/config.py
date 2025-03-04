936244ba84f52a3a59a68971a7c74a3669c14506
UNIKerp <153507340+UNIKerp@users.noreply.github.com>
2025-03-04 12:35:24 +0000

    acces

--- integration_sah/models/config.py
+++ integration_sah/models/config.py
@@ -8,6 +8,8 @@ class ConfigSAH(models.Model):
 
     _inherit = "res.company"
 
-    token_sah = fields.Char(string="Token")
-    secret_key_sah = fields.Char(string="Secret Key")
-    
+    token_sah = fields.Char(string="Token",copy=False)
+    secret_key_sah = fields.Char(string="Secret Key",copy=False)
+    url_commande = fields.Char(string='URL commande SAH', copy=False, default='https://demoapi.sellingathome.com/v1/Orders')
+    date_debut = fields.Datetime(string="Date de debut",copy=False)
+    date_fin = fields.Datetime(string="Date de Fin",copy=False)
--- integration_sah/models/sale_sak_mapping.py
+++ integration_sah/models/sale_sak_mapping.py
@@ -7,15 +7,6 @@ from odoo.exceptions import ValidationError
 
 _logger = logging.getLogger(__name__)
 
-class ConfigAccesSAH(models.Model):
-    _name = "acces.sah"
-    _description = "Configuration des acces des commandes de SAH"
-
-    url_commande = fields.Char(string='URL commande SAH', copy=False, default='https://demoapi.sellingathome.com/v1/Orders')
-    date_debut = fields.Datetime(string="Date de debut",copy=False)
-    date_fin = fields.Datetime(string="Date de Fin",copy=False)
-
-
 class MappingSAHOdoo(models.Model):
     _name = "commande.sah"
     _description = "Table pour stoker les commandes de SAH dans Odoo"
