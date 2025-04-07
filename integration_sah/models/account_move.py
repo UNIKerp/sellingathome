a560d17c63c69c83464a05ea4a3a39258215d237
sbadiane <159531370+sbadiane@users.noreply.github.com>
2025-04-07 16:22:16 +0000

    tt

--- integration_sah/models/account_move.py
+++ integration_sah/models/account_move.py
@@ -14,36 +14,40 @@ class AccountMove(models.Model):
 
 
 
-    def action_post(self):
-        _logger.info('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
-        res = super().action_post()
-        _logger.info("hhhhhhhhhhhhhhhhhhh%s",res)
-        for move in res:
-            _logger.info('mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm %s',move)
-            move.action_invoice_sent()
-            move.action_print_pdf()
-            _logger.info('okkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk')
-
     # def action_post(self):
-    #     # Disabled by default to avoid breaking automated action flow
-    #     action_invoice_sent()
-    #     if (
-    #         not self.env.context.get('disable_abnormal_invoice_detection', True)
-    #         and self.filtered(lambda m: m.abnormal_amount_warning or m.abnormal_date_warning)
-    #     ):
-    #         wizard = self.env['validate.account.move'].create({
-    #             'move_ids': [Command.set(self.ids)],
-    #         })
-    #         return {
-    #             'name': _("Confirm Entries"),
-    #             'type': 'ir.actions.act_window',
-    #             'res_model': 'validate.account.move',
-    #             'res_id': wizard.id,
-    #             'view_mode': 'form',
-    #             'target': 'new',
-    #         }
-    #     if self:
-    #         self._post(soft=False)
-    #     if autopost_bills_wizard := self._show_autopost_bills_wizard():
-    #         return autopost_bills_wizard
-    #     return False
+    #     _logger.info('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
+    #     res = super().action_post()
+    #     _logger.info("hhhhhhhhhhhhhhhhhhh%s",res)
+    #     for move in res:
+    #         _logger.info('mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm %s',move)
+    #         move.action_invoice_sent()
+    #         move.action_print_pdf()
+    #         _logger.info('okkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk')
+
+    def action_post(self):
+        # Disabled by default to avoid breaking automated action flow
+    
+        _logger.info('mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm %s',self)
+        self.action_invoice_sent()
+        self.action_print_pdf()
+        _logger.info('okkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk')
+        if (
+            not self.env.context.get('disable_abnormal_invoice_detection', True)
+            and self.filtered(lambda m: m.abnormal_amount_warning or m.abnormal_date_warning)
+        ):
+            wizard = self.env['validate.account.move'].create({
+                'move_ids': [Command.set(self.ids)],
+            })
+            return {
+                'name': _("Confirm Entries"),
+                'type': 'ir.actions.act_window',
+                'res_model': 'validate.account.move',
+                'res_id': wizard.id,
+                'view_mode': 'form',
+                'target': 'new',
+            }
+        if self:
+            self._post(soft=False)
+        if autopost_bills_wizard := self._show_autopost_bills_wizard():
+            return autopost_bills_wizard
+        return False
