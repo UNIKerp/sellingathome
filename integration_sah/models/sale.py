a02aa2090e73ec184ab656cf6095e835f387a9b3
aliadilanielfayaz <57450848+aliadilanielfayaz@users.noreply.github.com>
2025-05-06 11:09:37 +0000

    commente

--- integration_sah/models/sale.py
+++ integration_sah/models/sale.py
@@ -29,11 +29,24 @@ class SaleSAH(models.Model):
 
     
     def action_confirm(self):
+        """
+            Redéfinit la méthode action_confirm de sale.order pour automatiser la création et la confirmation de factures.
+
+            Cette méthode est appelée lorsqu'un utilisateur confirme une commande de vente.  Elle effectue les actions suivantes :
+            1. Appelle la méthode action_confirm de la classe parente (sale.order) pour exécuter la logique standard de confirmation de commande.
+            2. Pour chaque commande dans le self (qui peut contenir plusieurs commandes si l'utilisateur en confirme plusieurs en même temps):
+                3. Vérifie si la commande est prête à être facturée (invoice_status == 'to invoice').
+                4. Si la commande est prête à être facturée, elle crée une nouvelle facture (account.move) avec les détails de la commande.
+                5. Confirme (valide) la facture nouvellement créée.
+            6. Retourne le résultat de l'appel à la méthode de la classe parente.
+        """    
         res = super(SaleSAH, self).action_confirm()
+        # Itère sur chaque commande dans le self.  self peut contenir plusieurs commandes si l'utilisateur
+        # sélectionne et confirme plusieurs commandes en même temps dans l'interface utilisateur
         for order in self:
             if order.invoice_status == 'to invoice':
                 invoice = self.env['account.move'].create({
-                    'move_type': 'out_invoice',
+                    'move_type': 'out_invoice', # Définit le type de la facture comme "Facture Client".
                     'partner_id': order.partner_id.id,
                     'currency_id': order.pricelist_id.currency_id.id,
                     'invoice_line_ids': [(0, 0, {
@@ -44,12 +57,13 @@ class SaleSAH(models.Model):
                         'product_id': line.product_id.id,
                         'tax_ids': [(6, 0, line.tax_id.ids)],
                         'sale_line_ids': [(6, 0, [line.id])],
-                    }) for line in order.order_line],
+                    }) for line in order.order_line], # order.order_line est l'ensemble des lignes de la commande
                     'invoice_origin': order.name,
                     'ref': order.client_order_ref,
                     'company_id': order.company_id.id,
                     'user_id': order.user_id.id,
                 })
+                # Confirme (valide) la facture nouvellement créée.
                 invoice.action_post()
         return res
 
