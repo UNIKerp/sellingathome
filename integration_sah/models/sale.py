4f6c30d0f2c6ac6069a493dde83f05c2df82beff
sbadiane <159531370+sbadiane@users.noreply.github.com>
2025-05-08 15:41:46 +0000

    tab_produit_ids

--- integration_sah/models/sale.py
+++ integration_sah/models/sale.py
@@ -25,12 +25,12 @@ class SaleLineSAH(models.Model):
             if produits:
                 for produit in produits:
                     tab_produit_ids.append(produit.id)
-            self.produit_available_ids = tab_produit_ids
+            self.produit_available_ids.append(tab_produit_ids)
         else:
             if pro:
                 for p in pro:
                     tab_produit_ids.append(p)
-            self.produit_available_ids = tab_produit_ids
+            self.produit_available_ids.append(tab_produit_ids) 
 
     
 
