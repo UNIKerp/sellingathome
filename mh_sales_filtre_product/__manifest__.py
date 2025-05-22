f9c584d3f54435b2f91efa434ae5da75e7cf5b76
sbadiane <sbadiane@unikerp.com>
2025-05-22 15:42:38 +0000

    mh

---
+++ mh_sales_filtre_product/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "MH-sales-filtre-product",
+    "version": "18.0.0.2",
+    "category": "Sales",
+    "author": "INSPIRIT-TECH",
+    "website": "https://inspirit-tech.com/",
+    "license": "Other proprietary",
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+    "depends": [
+        "base", "sale"
+    ],
+    "data": [ 
+        'views/sale_order_views.xml',         
+    ],
+    "qweb": [
+        
+    ],
+    "js": [
+        
+    ],
+    "demo": [
+        
+    ],
+    "summary": "",
+    "description": """
+
+
+
+    """
+}
